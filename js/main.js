fa4244b56d0d5fb58053e21638aff6e114ad3e64
Surya C. A <suryac.akasam@gmail.com>
2015-11-18 17:12:32 +0530

    updated with Sobins code

--- js/main.js
+++ js/main.js
@@ -1,41 +1,61 @@
-//Player Template
-var Player = function(name,dice)
-{
-  this.name = name;
-  this.dice = dice;
-};
-var Tile = function(rows,cols){
-  this.rows = rows;
-  this.cols = cols;
-};
-// Object to take the parameters and create a Board with tiles
-var Board = function(rows , cols){
-  this.tiles = [];
-  this.rows = rows;
-  this.cols = cols;
-  this.init = function(){ //function to initialize the board
-    for(i=0;i<rows;i++)
-      for(j=0;j<cols;j++)
-        this.tiles.push(new Tile(i,j));
-  };
-  this.drawBoard = function(){ //function to Draw the board with tiles on HTML
-    console.log(this.tiles);
-  for(var i = 0; i< this.tiles.length; i++){
-      tileDiv = document.createElement('div'); //creating div element
-      tileDiv.className = ('tile'); //setting class name
-      tileDiv.innerHTML = '<span>' + "hi" + '</span>';
-      tileDiv.addEventListener('click', function(event){
-        //document.body.appendChild(tileDiv);
-      });
-    document.body.appendChild(tileDiv);
-  }
-  };
-};
-window.onload = function(){
+var tile = function(r,c){
+  this.row = r;
+  this.col = c;
 
-  playerOne = new Player("Surya","X");
-  playerTwo = new Player("Priya","O");
-  board = new Board(3,3);
-  board.init();
-  board.drawBoard();
 };
+
+var player = function(name, sign){
+  this.name = name;
+  this.sign = sign
+};
+var players =[];
+players[0]= new player('Surya','X');
+players[1]= new player('Darshan','0');
+var playerCounter=0;
+
+var board = function(){
+  this.tiles =[];
+  //create tic and toe tile array
+  this.init = function(){
+    for(var i = 0; i<3; i++)
+      for(var j=0; j<3; j++)
+        this.tiles.push(new tile(i,j));
+  };
+  //Draw the tic tac toc board
+  this.drawBoard = function(){
+    var boardDiv = document.createElement('div');
+    boardDiv.className = 'board';
+    boardDiv.setAttribute('id','board');
+    var row=0;
+    var rowDiv= [];
+    for(var i = 0; i<3; i++)
+    {
+      rowDiv[i] = document.createElement('div');
+      rowDiv[i].className = 'row';
+    }
+    //9 tiles
+    for(var i = 0; i<this.tiles.length; i++)
+    {
+      var tileDiv = document.createElement('span');
+      tileDiv.className = 'tile';
+      tileDiv.setAttribute('id','tile['+i+']');
+      tileDiv.setAttribute('data-row',this.tiles[i].row);
+      tileDiv.setAttribute('data-col',this.tiles[i].col);
+      tileDiv.addEventListener('click',function(){
+        this.innerHTML = players[playerCounter].sign;
+        playerCounter= 1- playerCounter;
+      });
+      rowDiv[row].appendChild(tileDiv);
+      if(i==2 || i==5 || i==8)
+        row++;
+    }
+    for(var i = 0; i<3; i++)
+    {
+      boardDiv.appendChild(rowDiv[i]);
+    }
+    document.body.appendChild(boardDiv);
+  };
+};
+var myBoard = new board();
+myBoard.init();
+myBoard.drawBoard();
