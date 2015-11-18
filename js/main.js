ffae71af29f3f09293ad2112ec7037f3c3c31822
Surya C. A <suryac.akasam@gmail.com>
2015-11-18 10:51:33 +0530

    changed Windows Onload

--- js/main.js
+++ js/main.js
@@ -3,23 +3,22 @@ var Player = function(name,dice)
 {
   this.name = name;
   this.dice = dice;
-
 };
 
-// Object to take the parameters and create a Board with tiles 
-var Boards = function(rows , columns)
-{
-	this.rows = rows;
-	this.cols = columns;
-	this.drawTiles()
-	{ 
-		for(var i = 0; i< rows;row++)
-		{
-			for(var j = 0; j< cols; cols++)
-			{
-				//Logic to draws tiles
+// Object to take the parameters and create a Board with tiles
+//window.onload() = function(rows = 3, columns = 3)
+window.onload = function(){
+alert("hi");
+  this.rows = 3;
+	this.cols = 4;
+  //function to draw tiles
+  //this.drawTiles(){
+		for(var i = 0; i< rows;row++){
+			for(var j = 0; j< cols; cols++){
+          div = document.createElement('div');
+          div.classname = 'tile'
+
 			}
 		}
-	}
+	//}
 };
-
