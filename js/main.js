5b59d255408b06a2b7ed586b4cd36d048a0593c1
Surya C. A <suryac.akasam@gmail.com>
2015-11-18 12:59:43 +0530

    modied the Draw board function

--- js/main.js
+++ js/main.js
@@ -4,25 +4,37 @@ var Player = function(name,dice)
   this.name = name;
   this.dice = dice;
 };
-var Tile = funtion(row,cols);{
+var Tile = function(row,cols){
   this.rows = rows;
   this.cols = cols;
-}
+};
 // Object to take the parameters and create a Board with tiles
-var Board = function(){
+var Board = function(rows , cols){
   this.tiles = [];
+  this.rows = rows;
+  this.cols = cols;
   this.init = function(){ //function to initialize the board
-    for(i=0;i<row;i++)
+    for(i=0;i<rows;i++)
       for(j=0;j<cols;j++)
         this.tiles.push(new Tile(i,j));
   };
   this.drawBoard = function(){ //function to Draw the board with tiles on HTML
-  for(var i = 0; i< this.tiles.length; i++){
-      tileDiv = document.createElement('div'); //creating div element
-      tileDiv.className = 'tile'; //setting class name
-      tileDiv.addEventListener('click', function(event){ // adding event listener
-      document.body.appendChild(tileDiv)
-      }
-    };
-  };
+    console.log()
+  // for(var i = 0; i< this.tiles.length; i++){
+  //     tileDiv = document.createElement('div'); //creating div element
+  //     tileDiv.className = ('tile'); //setting class name
+  //     tileDiv.innerHTML = '<span>' + "hi" + '</span>'
+  //     tileDiv.addEventListener('click', function(event){
+  //       document.body.appendChild(tileDiv);
+  //     });
+  // }
+  // };
+};
+window.onload = function(){
+
+  playerOne = new Player("Surya","X");
+  playerTwo = new Player("Priya","O");
+  board = new Board(3,3);
+  board.drawBoard();
+
 };
